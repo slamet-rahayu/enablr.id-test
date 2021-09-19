24c598859eaca759aee72f15534c63be1ba897c2
slamet-rahayu <slamet_rahayu93@yahoo.com>
2021-09-19 11:46:15 +0700

    fix backend form

--- pages/api/form.js
+++ pages/api/form.js
@@ -6,19 +6,22 @@ import validate from 'backend-util/middleware/form';
 const handler = nextConnect();
 
 const currentDir = process.cwd();
-const dataPath = path.join(currentDir, '/public/data/data.txt');
+const dataPath = path.join(currentDir, '/backend-util/data/data.txt');
 
 handler.get(async (req, res) => {
   try {
     const data = fs.readFileSync(dataPath);
     res.send(data.toString());
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send('Error: Submit data first');
   }
 });
 
 handler.use(validate).post(async (req, res) => {
   try {
+    if (!fs.existsSync(dataPath)) {
+      fs.writeFileSync(dataPath, '');
+    }
     const stream = fs.createWriteStream(dataPath);
     stream.once('open', () => {
       stream.write(`${req.body.fullname}\n`);
@@ -28,6 +31,7 @@ handler.use(validate).post(async (req, res) => {
     });
     res.send('success');
   } catch (error) {
+    console.log(error.message);
     res.status(500).send('Internal Server Error');
   }
 });

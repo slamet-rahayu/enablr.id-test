3dc83c4b51c943b12c01494df38009f0ab6719ae
slamet-rahayu <slamet_rahayu93@yahoo.com>
2021-09-19 11:10:26 +0700

    debug backend

--- pages/api/form.js
+++ pages/api/form.js
@@ -13,7 +13,7 @@ handler.get(async (req, res) => {
     const data = fs.readFileSync(dataPath);
     res.send(data);
   } catch (error) {
-    res.status(500).send('Internal Server Error');
+    res.status(500).send(error.message);
   }
 });
 
